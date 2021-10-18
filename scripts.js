cff3d5eb8cec2e0fec5e89f9e10a4171b80ff253
Rob <klassen.rob@gmail.com>
2021-10-17 21:23:21 -0400

    a potential avenue for solution, trying something more general

--- scripts.js
+++ scripts.js
@@ -1,35 +1,41 @@
 const app = {};
 
 
+
+
+
+
+
+app.toggleClasses = function(target, classToAdd, classToRemove){
+    target.classList.add(classToAdd);
+    target.classList.remove(classToRemove);
+    console.log("removed", classToRemove);
+}
+
 app.slideOutHandler = function(){
     const slideoutButton = document.querySelector("#sideNavButton");
     const slideoutPage = document.querySelector("#sideNavSlideout");
-    let slideOutToggle = false;
+    const projectDropdown = document.querySelector('#slideoutProjectList');
+    const sideNavSlideoutContact = document.querySelector("#sideNavSlideoutContact");
 
-    // const toggleClasses = function(target, classToAdd, classToRemove){
-    //     target.classList.add(classToAdd);
-    //     target.classList.add(classToRemove);
-    //     console.log("removed", classToRemove);
-    // }
+    let slideOutToggle = false;
+    let projectDropdownToggle = false;
+
+    projectDropdown.addEventListener('click', function(){
+        app.toggleClasses(projectDropdown.children, "", "");
+        app.toggleClasses(sideNavSlideoutContact, "", "")
+    })
 
     slideoutButton.addEventListener('click', function(){
         console.log("you clicked it", slideOutToggle);
         slideOutToggle = !slideOutToggle
         if (slideOutToggle === true){
-            // toggleClasses(slideoutPage, "showSlideout", "hideSlideout");
-            // toggleClasses(slideoutButton, "fullCircle", "emptyCircle");
-            slideoutPage.classList.remove("hideSlideout");
-            slideoutPage.classList.add("showSlideout");
-            slideoutButton.classList.remove("emptyCircle");
-            slideoutButton.classList.add("fullCircle");
+            app.toggleClasses(slideoutPage, "showSlideout", "hideSlideout");
+            app.toggleClasses(slideoutButton, "fullCircle", "emptyCircle");
         } else if (slideOutToggle === false) {
-            // toggleClasses(slideoutPage, "hideSlideout", "showSlideout");
-            // toggleClasses(slideoutButton,"emptyCircle", "fullCircle");
-            slideoutPage.classList.remove("showSlideout");
-            slideoutPage.classList.add("hideSlideout");
-            slideoutButton.classList.remove("fullCircle");
-            slideoutButton.classList.add("emptyCircle");
-
+            app.toggleClasses(slideoutPage, "hideSlideout", "showSlideout");
+            app.toggleClasses(slideoutButton,"emptyCircle", "fullCircle");
+            projectDropdownToggle = false;
         }
     });
 }
@@ -46,3 +52,34 @@ app.init = function(){
 }
 
 app.init();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// slideoutButton.addEventListener('click', function(){
+//     console.log("you clicked it", slideOutToggle);
+//     slideOutToggle = !slideOutToggle
+//     if (slideOutToggle === true){
+//         // slideoutPage.classList.remove("hideSlideout");
+//         // slideoutPage.classList.add("showSlideout");
+//         // slideoutButton.classList.remove("emptyCircle");
+//         // slideoutButton.classList.add("fullCircle");
+//     } else if (slideOutToggle === false) {
+//         // slideoutPage.classList.remove("showSlideout");
+//         // slideoutPage.classList.add("hideSlideout");
+//         // slideoutButton.classList.add("emptyCircle");
+//         // slideoutButton.classList.remove("fullCircle");
+
+//     }
+// });
+// }
